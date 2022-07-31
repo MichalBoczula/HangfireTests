7962702a1d35907b484a2d248ca4be6debc3b6de
Michal Boczula <michal.boczula0101@gmail.com>
2022-07-31 21:18:22 +0200

    Add project files.

---
+++ Hangfire.App/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangfire.App
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddServiceWorker(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddHangfire(x =>
+            {
+                x.UseSqlServerStorage(configuration.GetConnectionString("DefaultConnection"));
+            });
+            services.AddHangfireServer();
+
+            services.AddHostedService<Worker>();
+
+            return services;
+        }
+    }
+}
---
+++ Hangfire.App/Worker.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Hangfire.App
+{
+    public class Worker : BackgroundService
+    {
+        private readonly IRecurringJobManager _recurringJobManager;
+
+        public Worker(IRecurringJobManager recurringJobManager)
+        {
+            _recurringJobManager = recurringJobManager;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _recurringJobManager.AddOrUpdate("jobId", () => Debug.WriteLine("Drugi Test Ziom _________________________________________"), Cron.Minutely);
+        }
+    }
+}
